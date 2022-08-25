874fd73db3c7bc30dcbfa14dc023cfdba2c1b1b0
swoter <iswoter@gmail.com>
2022-08-25 13:24:32 +0800

    http-api-dynamodb test pass

--- http-api-dynamodb/deploy.sh
+++ http-api-dynamodb/deploy.sh
@@ -5,4 +5,4 @@ echo "Deploy to Stage $ENVPROFILE"
 echo "##############################################"
 
 # deploy using serverless
-AWS_PROFILE=$ENVPROFILE sls deploy --stage=$ENVPROFILE
+AWS_PROFILE=$ENVPROFILE sls deploy --stage=$ENVPROFILE --force
